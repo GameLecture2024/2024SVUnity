b2c73eb8b4c6d9a04c7ba4562cd4d82b0923aee1
ksw617 <dlfjswodlf@naver.com>
2024-07-26 15:49:53 +0900

    2024-07-26 방특 수업3_ 레벨 설정 및 캐릭터

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,27 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    CharacterController characterController;
-    public float moveSpeed = 10;
-    // Start is called before the first frame update
-    void Start()
+    // 주석.
+    // 초심자가 실수하기 쉬운 가장 많은 녀석 첫번째, 대소문자 오탈자
+    public CharacterController characterController; // 초록색 이름 타입 - 흰색 이름 변수
+    public float speed = 10f;                       // 초기 속도를 10으로 설정한다.
+
+    private void Start()
     {
-        characterController = GetComponent<CharacterController>();
+        
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        PlayerMove();
-    }
-
-    void PlayerMove()
-    {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-
-        Vector3 moveDir = new Vector3(horizontal, 0, vertical).normalized;
-
-        characterController.SimpleMove(moveDir * moveSpeed);
+        characterController.SimpleMove(Vector3.forward * speed);
     }
 }
