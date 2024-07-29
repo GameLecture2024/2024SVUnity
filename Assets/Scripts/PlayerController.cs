31a77e4336151c9dbad13cbc20c52da58515f35a
ksw617 <dlfjswodlf@naver.com>
2024-07-29 15:54:50 +0900

    (2024.07.29) 플레이어 이동 구현

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,8 +14,40 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    private void Update()
+    private void Update() // 움직임은 계속 변화하는 것이기 때문에 Update 작성해야겠다.
     {
-        characterController.SimpleMove(Vector3.forward * speed);
+        PlayerMove();
+        PlayerCameraMove();
     }
+
+    private void PlayerMove()
+    {
+        // 플레이어의 입력을 받고 싶다.
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Debug.Log($"horizontalInput : {horizontalInput}, verticlaInput : {verticalInput}");
+
+        Vector3 moveVector = new Vector3(horizontalInput, 0, verticalInput);
+
+        // Time.deltaTime연산이 내부적으로 처리되어 있기 때문에 추가안해줘도 된다.
+        characterController.SimpleMove(moveVector * speed); // 방향과 속도 speed Vector
+    }
+
+    // 시점을 움직이고 싶다.
+    private void PlayerCameraMove()
+    {
+        // 플레이어의 입력. Mouse 입력을받아야겠죠
+        float MouseX = Input.GetAxis("Mouse X");
+
+        Debug.Log($"MouseX {MouseX}");
+        float MouseY = Input.GetAxis("Mouse Y");
+
+        // transform 각도. Y축을 MouseX값만큼 변경해주면 된다.
+
+        transform.rotation = Quaternion.Euler(MouseX, MouseY, 0);
+
+    }
+
+
 }
